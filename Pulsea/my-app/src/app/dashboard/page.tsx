6da6ebd53acc29d0e995889e17dfcdf0b67c2227
Mohit Thapa <thapamohit2058@gmail.com>
2025-11-01 22:40:25 +0545

    drizzle setup done with supabase

--- Pulsea/my-app/src/app/dashboard/page.tsx
+++ Pulsea/my-app/src/app/dashboard/page.tsx
@@ -1,10 +1,14 @@
 import { Container } from "@/components/container";
 import NewProjectButton from "@/components/newproject-btn";
-import { Button } from "@/components/ui/button";
+import { db } from "@/db";
+import { Projects } from "@/db/schema";
+
 import { SignOutButton } from "@clerk/nextjs";
 import Link from "next/link";
 
-const Page = () => {
+const Page = async () => {
+  const allProjects = await db.select().from(Projects);
+  console.log(allProjects);
   return (
     <Container>
       <header className="my-2 flex items-center justify-between">
