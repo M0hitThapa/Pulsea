07ac0c0787de4c906b699bd54a604a6623326e68
Mohit Thapa <thapamohit2058@gmail.com>
2025-11-12 19:14:05 +0545

    added description and link place

--- Pulsea/my-app/src/app/(user)/projects/[projectId]/page.tsx
+++ Pulsea/my-app/src/app/(user)/projects/[projectId]/page.tsx
@@ -1,6 +1,7 @@
 import { db } from "@/db";
 import { projects as dbProjects } from "@/db/schema";
 import { eq } from "drizzle-orm";
+import Link from "next/link";
 
 const Page = async ({
   params,
@@ -29,7 +30,13 @@ const Page = async ({
   }
 
   console.log(project);
-  return <div>{project.name}</div>;
+  return (
+    <div>
+      <h1>{project.name}</h1>
+      <h2>{project.description}</h2>
+      {project.url ? <Link href={project.url}>Visit site</Link> : null}
+    </div>
+  );
 };
 
 export default Page;
