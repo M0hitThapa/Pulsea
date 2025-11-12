d92abc72657c8ce179bd6892653e22bcaad14363
Mohit Thapa <thapamohit2058@gmail.com>
2025-11-12 18:31:58 +0545

    made schema for feedback with defining its relation

--- Pulsea/my-app/src/db/schema.ts
+++ Pulsea/my-app/src/db/schema.ts
@@ -1,4 +1,5 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { integer, pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
 
 export const Projects = pgTable("projects", {
   id: serial("id").primaryKey(),
@@ -7,3 +8,22 @@ export const Projects = pgTable("projects", {
   url: text("url"),
   userId: varchar("user_id"),
 });
+
+export const feedbacks = pgTable("feedback", {
+  id: serial("id").primaryKey(),
+  projectId: integer("project_id"),
+  userName: text("user_name"),
+  userEmail: text("user_email"),
+  message: text("message"),
+});
+
+export const ProjectRelation = relations(Projects, ({ many }) => ({
+  feedbacks: many(feedbacks),
+}));
+
+export const FeedbackRelations = relations(feedbacks, ({ one }) => ({
+  project: one(Projects, {
+    fields: [feedbacks.projectId],
+    references: [Projects.id],
+  }),
+}));
